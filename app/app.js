a55f419cb2912133cba62d0dff1603b3b87a939d
bellsilver7 <bellsilver7@gmail.com>
2022-06-20 23:36:32 +0900

    directory optimization

--- app/app.js
+++ app/app.js
@@ -5,9 +5,9 @@ const express = require("express");
 const app = express();
 
 // 라우팅
-const route = require("./routes/");
+const route = require("./src/routes");
 
-app.set("views", "./views");
+app.set("views", "./src/views");
 app.set("view engine", "ejs");
 
 app.use("/", route);

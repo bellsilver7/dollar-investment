1fb8ee6696b55b03d917ddb2e0389c60b959e907
bellsilver7 <bellsilver7@gmail.com>
2022-06-21 06:52:38 +0900

    Creating an object using the DOM

--- app/src/public/js/buy.js
+++ app/src/public/js/buy.js
@@ -1,5 +1,22 @@
 "use strict";
 
-console.log("hello");
+let currentPrice = 1300;
+const amountInput = document.querySelector("#amount");
+const totalPriceInput = document.querySelector("#amount");
+const buyBtn = document.querySelector("#buyBtn");
 
-console.log("bye");
+amountInput.addEventListener("keyup", (e) => {
+  const amount = e.target.value;
+  document.querySelector("#totalPrice").value = amount * currentPrice;
+});
+
+buyBtn.addEventListener("click", buy);
+function buy() {
+  const req = {
+    currentPrice: currentPrice,
+    amount: amountInput.value,
+    totalPrice: totalPriceInput.value,
+  };
+
+  console.log(req);
+}

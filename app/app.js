e4ab872737fd72c8d995f90cf4d05aed4b3c5f96
bellsilver7 <bellsilver7@gmail.com>
2022-06-21 06:36:12 +0900

    create javascript to front

--- app/app.js
+++ app/app.js
@@ -9,6 +9,7 @@ const route = require("./src/routes");
 
 app.set("views", "./src/views");
 app.set("view engine", "ejs");
+app.use(express.static(`${__dirname}/src/public`));
 
 app.use("/", route);
 

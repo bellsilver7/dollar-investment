88b5d536025d583819edc2780a09f7f9f177bc55
bellsilver7 <bellsilver7@gmail.com>
2022-06-25 18:58:18 +0900

    구름IDE MySQL 연동 및 dotenv 로 사용해 환경변수 등록

--- app/src/models/User.js
+++ app/src/models/User.js
@@ -9,15 +9,19 @@ class User {
 
   async login() {
     const client = this.body;
-    const { id, password } = await UserStorage.getUserInfo(client.userId);
+    try {
+      const { id, password } = await UserStorage.getUserInfo(client.userId);
 
-    if (id) {
-      if (id === client.userId && password === client.userPassword) {
-        return { success: true };
+      if (id) {
+        if (id === client.userId && password === client.userPassword) {
+          return { success: true };
+        }
+        return { success: false, message: "잘못된 비밀번호에요." };
       }
-      return { success: false, message: "잘못된 비밀번호에요." };
+      return { success: false, message: "존재하지 않는 아이디에요." };
+    } catch (error) {
+      return { success: false, message: error };
     }
-    return { success: false, message: "존재하지 않는 아이디에요." };
   }
 
   async register() {

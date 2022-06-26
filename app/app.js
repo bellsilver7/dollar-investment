b1180525d85db5b2b62710e5b011e0e6876573a8
bellsilver7 <bellsilver7@gmail.com>
2022-06-26 11:02:17 +0900

    morgan 모듈을 이용한 로그 관리

--- app/app.js
+++ app/app.js
@@ -4,9 +4,12 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const dotenv = require("dotenv");
-dotenv.config();
+const morgan = require("morgan");
 
 const app = express();
+dotenv.config();
+
+const accessLogStream = require("./src/config/log");
 
 // 라우팅
 const route = require("./src/routes");
@@ -16,6 +19,15 @@ app.set("view engine", "ejs");
 app.use(express.static(`${__dirname}/src/public`));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(morgan("tiny"));
+// app.use(
+//   morgan(":method :url :status :res[content-length] - :response-time ms")
+// );
+// app.use(morgan("combined"));
+// app.use(morgan("dev", { stream: accessLogStream }));
+app.use(morgan("common", { stream: accessLogStream }));
+// app.use(morgan(":method :date[web]", { stream: accessLogStream }));
+// mongoose-moran 을 사용하면 MongoDB 와 연결 가능
 
 app.use("/", route);
 

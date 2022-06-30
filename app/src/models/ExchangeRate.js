f62b68950cddc79667a754a25cc049295f99febe
bellsilver7 <bellsilver7@gmail.com>
2022-07-01 07:48:20 +0900

    환율조회 URL 호출 로그

--- app/src/models/ExchangeRate.js
+++ app/src/models/ExchangeRate.js
@@ -13,14 +13,20 @@ class ExchangeRate {
       "https://www.koreaexim.go.kr/site/program/financial/exchangeJSON";
     const EXCHANGE_INFO_CODE = "AP01";
     const DOLLAR_CODE = "USD";
+
+    console.log(
+      `${API_URL}` +
+        `?authkey=${process.env.API_KEY}` +
+        `&searchdate=${client.searchDate}` +
+        `&data=${EXCHANGE_INFO_CODE}`
+    );
     try {
-      const searchDate = client.searchDate;
       const info = await axios({
         method: "GET",
         url:
           `${API_URL}` +
           `?authkey=${process.env.API_KEY}` +
-          `&searchdate=${searchDate}` +
+          `&searchdate=${client.searchDate}` +
           `&data=${EXCHANGE_INFO_CODE}`,
       }).then((res) => {
         return res.data.filter((item) => item.cur_unit === DOLLAR_CODE);

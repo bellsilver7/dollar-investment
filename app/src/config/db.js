1d9e138695fda6d0279b83dd951fc4f6ee532538
bellsilver7 <bellsilver7@gmail.com>
2022-09-26 22:21:27 +0900

    🔧 sqlite db설정

--- app/src/config/db.js
+++ app/src/config/db.js
@@ -1,13 +1,30 @@
 const mysql = require("mysql");
+const sqlite3 = require("sqlite3").verbose();
 
-const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
+// open database in memory
+const db = new sqlite3.Database("../database/database.sqlite", (error) => {
+  if (error) {
+    console.error(error.message);
+  }
+  console.log("Connected to the database.");
 });
 
-db.connect();
+// close the database connection
+db.close((error) => {
+  if (error) {
+    return console.error(error.message);
+  }
+  console.log("Close the database connection.");
+});
+
+// const db = mysql.createConnection({
+//   host: process.env.DB_HOST,
+//   port: process.env.DB_PORT,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASS,
+//   database: process.env.DB_NAME,
+// });
+
+// db.connect();
 
 module.exports = db;

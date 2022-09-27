07693ed231d10049b3f6d8259fa15b0cd5526764
bellsilver7 <bellsilver7@gmail.com>
2022-09-28 07:36:21 +0900

    knex db 설정

--- app/src/config/db.js
+++ app/src/config/db.js
@@ -1,30 +1,14 @@
-const mysql = require("mysql");
-const sqlite3 = require("sqlite3").verbose();
+const knex = require("knex");
+const config = {
+  development: {
+    client: "sqlite3",
+    connection: {
+      filename: "./database.sqlite",
+    },
+    useNullAsDefault: true,
+  },
+};
 
-// open database in memory
-const db = new sqlite3.Database("../database/database.sqlite", (error) => {
-  if (error) {
-    console.error(error.message);
-  }
-  console.log("Connected to the database.");
-});
-
-// close the database connection
-db.close((error) => {
-  if (error) {
-    return console.error(error.message);
-  }
-  console.log("Close the database connection.");
-});
-
-// const db = mysql.createConnection({
-//   host: process.env.DB_HOST,
-//   port: process.env.DB_PORT,
-//   user: process.env.DB_USER,
-//   password: process.env.DB_PASS,
-//   database: process.env.DB_NAME,
-// });
-
-// db.connect();
+const db = knex(config.development);
 
 module.exports = db;

01afa331088108d66207ac837825dc760f30c988
bellsilver7 <bellsilver7@gmail.com>
2022-09-28 07:37:22 +0900

    ⚡️ UserStorage knex 적용

--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -3,28 +3,14 @@
 const db = require("../config/db");
 
 class UserStorage {
-  static getUserInfo(id) {
-    return new Promise((resolve, reject) => {
-      const query = "select * from users where id = ?;";
-      db.query(query, [id], (error, data) => {
-        if (error) return reject(`${error}`);
-        return resolve(data[0]);
-      });
-    });
+  static async getUserInfo(id) {
+    const data = await db("users").where("id", id);
+    return data[0];
   }
 
   static async save(userInfo) {
-    return new Promise((resolve, reject) => {
-      const query = "insert into users(id, name, password) values(?, ?, ?); ";
-      db.query(
-        query,
-        [userInfo.id, userInfo.name, userInfo.password],
-        (error) => {
-          if (error) return reject(`${error}`);
-          return resolve({ success: true });
-        }
-      );
-    });
+    const data = await db("users").insert(userInfo);
+    return data[0];
   }
 }
 

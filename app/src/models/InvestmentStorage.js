7cf6eda5c2cc23462f008a3642480208145052ce
bellsilver7 <bellsilver7@gmail.com>
2022-09-29 08:05:57 +0900

    ⚡️ InvestmentStorage knex 적용

--- app/src/models/InvestmentStorage.js
+++ app/src/models/InvestmentStorage.js
@@ -1,32 +1,24 @@
 "use strict";
 
-const db = require("../config/db");
+const knex = require("../config/db");
 
 class InvestmentStorage {
   static async get() {
-    return new Promise((resolve, reject) => {
-      const query =
-        "select price, amount, created_at from investments order by created_at desc";
-      db.query(query, [], (error, data) => {
-        if (error) return reject(`${error}`);
-        return resolve(data);
-      });
-    });
+    return await knex
+      .select("price", "amount", "created_at")
+      .from("investments")
+      .orderBy("created_at", "desc");
   }
 
-  static async save(investmentInfo) {
-    return new Promise((resolve, reject) => {
-      const query =
-        "insert into investments(type, price, amount, created_at) values(?, ?, ?, now())";
-      db.query(
-        query,
-        [investmentInfo.type, investmentInfo.price, investmentInfo.amount],
-        (error) => {
-          if (error) return reject(`${error}`);
-          return resolve({ success: true });
-        }
-      );
-    });
+  static async save(data) {
+    const timestamp = Date.now();
+    const values = {
+      type: data.type,
+      price: data.price,
+      amount: data.amount,
+      created_at: timestamp,
+    };
+    return await knex.insert(values).into("investments");
   }
 }
 
